177344db703c40f8e9d0670a4b64b2acf60075b9
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-17 20:33:58 -0300

    added env vars to ECS task

--- variables.tf
+++ variables.tf
@@ -2,3 +2,33 @@ variable "region" {
   description = "AWS region"
   default     = "sa-east-1"
 }
+
+
+# Environment variables to ECS Task - It is stored on Terraform Cloud
+variable "URL_SANDBOX" {
+}
+
+variable "USERNAME" {
+}
+
+variable "PASSWORD" {
+}
+
+variable "TF_TOKEN" {
+}
+
+variable "TF_ACCESS_URL" {
+}
+
+variable "TF_ACCESS_ID" {
+}
+
+variable "TF_SECRET_URL" {
+}
+
+variable "TF_SECRET_ID" {
+}
+
+variable "QTD_SERVERS" {
+}
+
