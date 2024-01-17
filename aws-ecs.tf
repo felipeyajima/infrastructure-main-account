7f3661087d5045870865e40fdd414fc1aea58b2b
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-17 20:45:49 -0300

    Update aws-ecs.tf

--- aws-ecs.tf
+++ aws-ecs.tf
@@ -1,1 +1,23 @@
+resource "aws_ecs_task_definition" "turnon" {
+  family                   = "turnon"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "iis",
+    "image": "mcr.microsoft.com/windows/servercore/iis",
+    "cpu": 1024,
+    "memory": 2048,
+    "essential": true
+  }
+]
+TASK_DEFINITION
 
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
