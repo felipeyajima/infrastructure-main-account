a26e331728356e14731dedc09049de1b70dd2639
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-17 19:41:20 -0300

    Create aws-networking.tf

---
+++ aws-networking.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/22"
+}
+
+resource "aws_subnet" "public_sn" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "igwt" {
+  vpc_id = aws_vpc.main.id
+}
+
