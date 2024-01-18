d9c3331d9ca350509c98b2328c6cd01f55361358
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-17 22:17:03 -0300

    Update aws-ecs.tf

--- aws-ecs.tf
+++ aws-ecs.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_ecs_task_definition" "turnon" {
   family                   = "turnon"
   requires_compatibilities = ["FARGATE"]
@@ -12,12 +13,12 @@ resource "aws_ecs_task_definition" "turnon" {
     "cpu": 1024,
     "memory": 2048,
     "essential": true
-    #"environmentFiles": [
-    #  {
-    #    "value": "arn:aws:s3:::env-vars-ecs-yajima/ecs.env",
-    #    "type": "s3"
-    #  }
-    #]
+    "environmentFiles": [
+      {
+        "value": "arn:aws:s3:::env-vars-ecs-yajima/ecs.env",
+        "type": "s3"
+      }
+    ]
   }
 ]
 TASK_DEFINITION
@@ -28,4 +29,27 @@ TASK_DEFINITION
   }
 
 }
+*/
+resource "aws_ecs_task_definition" "test" {
+  family                   = "test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "iis",
+    "image": "mcr.microsoft.com/windows/servercore/iis",
+    "cpu": 1024,
+    "memory": 2048,
+    "essential": true
+  }
+]
+TASK_DEFINITION
 
+  runtime_platform {
+    operating_system_family = "WINDOWS_SERVER_2019_CORE"
+    cpu_architecture        = "X86_64"
+  }
+}
