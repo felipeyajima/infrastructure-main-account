54ec70f75af5f93a4fa3fde9b0124c9fb8f3c6ba
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-17 21:42:53 -0300

    Update aws-iam.tf

--- aws-iam.tf
+++ aws-iam.tf
@@ -1,7 +1,6 @@
-resource "aws_iam_policy" "policy-access-s3-vars" {
-  name        = "access-s3-vars"
-  path        = "/"
-  description = "Acesso aos arquivos de variaveis do ECS no S3"
+resource "aws_iam_role_policy" "policy-access-s3-env-bucket" {
+  name = "policy-access-s3-env-bucket"
+  role = aws_iam_role.role-ecs-to-s3.id
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
@@ -10,10 +9,32 @@ resource "aws_iam_policy" "policy-access-s3-vars" {
     Statement = [
       {
         Action = [
-          "s3:GetObject"
+          "s3:GetObject",
+          "s3:GetBucketLocation"
         ]
         Effect   = "Allow"
-        Resource = "*"
+        Resource = aws_s3_bucket.env-vars-ecs-yajima.arn
+      },
+    ]
+  })
+  depends_on = [
+     aws_s3_bucket.env-vars-ecs-yajima
+  ]
+}
+
+resource "aws_iam_role" "role-ecs-to-s3" {
+  name = "role-ecs-to-s3"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
       },
     ]
   })
