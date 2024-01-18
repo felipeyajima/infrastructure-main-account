0c879c40fd0402e9ce8f735c8d09dfdeab291979
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-17 21:12:34 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,8 @@ variable "region" {
 }
 
 
+
+
 # Environment variables to ECS Task - It is stored on Terraform Cloud
 variable "URL_SANDBOX" {
 }
@@ -30,5 +32,47 @@ variable "TF_SECRET_ID" {
 }
 
 variable "QTD_SERVERS" {
+}
+
+
+variable "ecr-vars"{
+  default = [
+        {
+          "name": "URL_SANDBOX",
+          "value": var.URL_SANDBOX
+        },
+        {
+          "name": "USERNAME",
+          "value": var.USERNAME
+        },
+        {
+          "name": "PASSWORD",
+          "value": var.PASSWORD
+        },
+        {
+          "name": "TF_TOKEN",
+          "value": var.TF_TOKEN
+        },
+        {
+          "name": "TF_ACCESS_URL",
+          "value": var.TF_ACCESS_URL
+        },
+        {
+          "name": "TF_ACCESS_ID",
+          "value": var.TF_ACCESS_ID
+        },
+        {
+          "name": "TF_SECRET_URL",
+          "value": var.TF_SECRET_URL
+        },
+        {
+          "name": "TF_SECRET_ID",
+          "value": var.TF_SECRET_ID
+        },
+        {
+          "name": "QTD_SERVERS",
+          "value": var.QTD_SERVERS
+        }
+      ]
 }
 
