da4441e33718ad2b5d0853ef60c377614ef8a5bf
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-23 19:59:07 -0300

    Update aws-networking.tf

--- aws-networking.tf
+++ aws-networking.tf
@@ -1,15 +1,24 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/22"
+  tags = {
+    Name = "portfolio"
+  }
 }
 
 resource "aws_subnet" "public_sn" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  tags = {
+    Name = "portfolio"
+  }
 }
 
 resource "aws_internet_gateway" "igwt" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "portfolio"
+  }
 }
 
 
@@ -26,3 +35,10 @@ resource "aws_route_table" "exit_to_igwt" {
   }
 }
 
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_sn.id
+  route_table_id = aws_route_table.exit_to_igwt.id
+  tags = {
+    Name = "portfolio"
+  }
+}
