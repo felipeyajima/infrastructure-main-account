50b6c29d46cfee22db7399377fac0fe455abcaf6
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-23 20:17:57 -0300

    Update aws-networking.tf

--- aws-networking.tf
+++ aws-networking.tf
@@ -5,6 +5,13 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_vpc" "wmain" {
+  cidr_block = "10.1.0.0/22"
+  tags = {
+    Name = "wportfolio"
+  }
+}
+
 resource "aws_subnet" "public_sn" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
