64ede0d62a6fa5789325b2096b7d17dec0fe8ece
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-23 19:12:20 -0300

    Update aws-sg.tf

--- aws-sg.tf
+++ aws-sg.tf
@@ -10,4 +10,11 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 }
 
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_internet" {
+  security_group_id = aws_security_group.allow_internet_turnon.id
 
+  cidr_ipv4   = "0.0.0.0/8"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
