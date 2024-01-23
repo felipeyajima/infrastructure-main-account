0c5c19f0531b0fa3167e64bb2a0762da273ff635
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-23 19:54:26 -0300

    Update aws-networking.tf

--- aws-networking.tf
+++ aws-networking.tf
@@ -14,15 +14,15 @@ resource "aws_internet_gateway" "igwt" {
 
 
 resource "aws_route_table" "exit_to_igtw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = "${aws_vpc.main.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igtw.id
+    gateway_id = "${aws_internet_gateway.igtw.id}"
   }
 
   tags = {
-    Name = "portifolio"
+    Name = "portfolio"
   }
 }
 
