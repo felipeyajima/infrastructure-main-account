ddae33c333fb04e07e4857fea963c0531a15a910
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-01-24 21:50:40 -0300

    destroying s3 bucket

--- aws-s3.tf
+++ aws-s3.tf
@@ -1,3 +1,6 @@
+## Deprecated - ECS isnt in use right now, and the env vars is not necessaria find on turnon.env
+/*
 resource "aws_s3_bucket" "env-vars-ecs-yajima" {
   bucket = "env-vars-ecs-yajima"
 }
+*/
