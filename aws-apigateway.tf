a2c68e5a05f13c1966731adc66d080a4f379bbd9
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-02-02 20:38:29 -0300

    Create aws-apigateway.tf

---
+++ aws-apigateway.tf
@@ -0,0 +1,17 @@
+resource "aws_api_gateway_rest_api" "main" {
+  name = "main"
+}
+
+resource "aws_api_gateway_resource" "turnon" {
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "turnon"
+  rest_api_id = aws_api_gateway_rest_api.main.id
+}
+
+resource "aws_api_gateway_method" "post-turnon" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.turnon.id
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+}
+
