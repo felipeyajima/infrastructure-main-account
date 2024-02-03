7e04311033d6313b893c6db1eafab0fbaa11e58e
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-02-02 21:25:03 -0300

    Update aws-apigateway.tf

--- aws-apigateway.tf
+++ aws-apigateway.tf
@@ -17,7 +17,7 @@ resource "aws_api_gateway_method" "post-turnon" {
   resource_id   = aws_api_gateway_resource.turnon.id
   rest_api_id   = aws_api_gateway_rest_api.main.id
 }
-/*
+
 resource "aws_api_gateway_integration" "to-batch" {
   http_method = aws_api_gateway_method.post-turnon.http_method
   resource_id = aws_api_gateway_resource.turnon.id
@@ -25,10 +25,10 @@ resource "aws_api_gateway_integration" "to-batch" {
   type        = "AWS"
   integration_http_method = "POST"
   uri = "arn:aws:apigateway:sa-east-1:batch:path/v1/submitjob"
-  #credentials = aws_iam_role.sqs_role.arn
+  credentials = aws_iam_role.portfolio-role-apigtw-to-services.arn
   request_templates = {                  # Not documented
     "application/json" = "${file("api_gateway_body_mapping.template")}"
   }
 }
-*/
+
 
